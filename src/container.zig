ae9b08e0bbda09c3ff32df09d67f24273d36443e
binarycraft007 <elliot.huang.signed@gmail.com>
2025-09-15 22:14:32 +0800

    add err_pipe for error reporting

--- src/container.zig
+++ src/container.zig
@@ -30,6 +30,22 @@ pub fn run(gpa: mem.Allocator, comptime function: anytype, args: anytype) !void 
     posix.sigaddset(&sigset, posix.SIG.TERM);
     posix.sigprocmask(posix.SIG.BLOCK, &sigset, &oldsigset);
 
+    const bad_fn_ret = "expected return type of startFn to be '!void'";
+    const ErrSet = switch (@typeInfo(@typeInfo(@TypeOf(function)).@"fn".return_type.?)) {
+        .error_union => |info| blk: {
+            if (info.payload != void) {
+                @compileError(bad_fn_ret);
+            }
+            break :blk info.error_set;
+        },
+        else => {
+            @compileError(bad_fn_ret);
+        },
+    };
+
+    const err_pipe: [2]posix.fd_t = try posix.pipe2(.{ .CLOEXEC = true });
+    //errdefer destroyPipe(err_pipe);
+
     const pid = try posix.fork();
     if (pid == 0) {
         posix.sigprocmask(posix.SIG.SETMASK, &oldsigset, null);
@@ -37,12 +53,22 @@ pub fn run(gpa: mem.Allocator, comptime function: anytype, args: anytype) !void 
         try posix.setuid(0);
         try posix.setgid(0);
 
-        @call(.auto, function, args) catch {
+        @call(.auto, function, args) catch |err| {
+            writeIntFd(err_pipe[1], @as(ErrInt, @intFromError(err))) catch {};
             posix.exit(1);
         };
         posix.exit(0);
     } else {
+        posix.close(err_pipe[1]); // make sure only the child holds the write end open
+        const err_fd = err_pipe[0];
         const status = posix.waitpid(pid, 0);
+        if (readIntFd(err_fd)) |child_err_int| {
+            posix.close(err_fd);
+            const err: ErrSet = @errorCast(@errorFromInt(child_err_int));
+            return err;
+        } else |_| {
+            posix.close(err_fd);
+        }
         switch (statusToTerm(status.status)) {
             .Signal => |sig| {
                 const term_sig: u8 = @intCast(sig);
@@ -56,6 +82,26 @@ pub fn run(gpa: mem.Allocator, comptime function: anytype, args: anytype) !void 
         }
     }
 }
+
+fn destroyPipe(pipe: [2]posix.fd_t) void {
+    if (pipe[0] != -1) posix.close(pipe[0]);
+    if (pipe[0] != pipe[1]) posix.close(pipe[1]);
+}
+
+fn writeIntFd(fd: i32, value: ErrInt) !void {
+    var buffer: [8]u8 = undefined;
+    var fw: std.fs.File.Writer = .initStreaming(.{ .handle = fd }, &buffer);
+    fw.interface.writeInt(u64, value, .little) catch unreachable;
+    fw.interface.flush() catch return error.SystemResources;
+}
+
+fn readIntFd(fd: i32) !ErrInt {
+    var buffer: [8]u8 = undefined;
+    var fr: std.fs.File.Reader = .initStreaming(.{ .handle = fd }, &buffer);
+    return @intCast(fr.interface.takeInt(u64, .little) catch return error.SystemResources);
+}
+
+const ErrInt = std.meta.Int(.unsigned, @sizeOf(anyerror) * 8);
 
 fn syncWithChild(fd: posix.fd_t, pid: posix.pid_t) !void {
     var buf: [8]u8 = undefined;
